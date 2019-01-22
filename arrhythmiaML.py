91876e98e383013eed2beec7729c53d43e54b597
Khalid Najm <khalidmnajm@gmail.com>
2019-01-22 14:14:16 +0000

    Classifying arrhythmia in patients using basic ML models
    This programme train/test on arrhthymia data from 452 patients and, using six different ML models, it will predict the classification of the severity of the arrhythmia (1-16) that the patients in the validation dataset experience.

---
+++ arrhythmiaML.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 22 10:51:44 2019
+
+@author: khalidnajm
+
+This programme train/test on arrhthymia data from 452 patients and, using six 
+different ML models, it will predict the classification of the severity of the 
+arrhythmia (1-16) that the patients in the validation dataset experice.
+"""
+
+# Load libraries
+import pandas
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset
+file_name = "/Users/khalidnajm/Documents/Data Science Projects/arrhythmia/arrhythmia.data.csv"
+dataset = pandas.read_csv(file_name, header=None, na_values='?')
+
+#the ninth row is mostly NaNs
+dataset = dataset.drop(9, axis=1)
+
+#drop all NaN rows
+dataset = dataset.dropna(axis=1)
+
+numbers = np.arange(1,275,1) #make the header numbers correspond to the numbers in the 'arrhythmia.names.txt' file
+dataset.columns = numbers
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:273]
+Y = array[:,273]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+# Test options and evaluation metric
+seed = 7
+scoring = 'accuracy'
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
+    
+# Compare Algorithms
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+
+# Make predictions on validation dataset
+knn = KNeighborsClassifier()
+knn.fit(X_train, Y_train)
+predictions = knn.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
